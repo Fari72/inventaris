e1258cc00480c2938d1299fe81ad906cd2f6aba6
Fari72 <fariaffrizal@gmail.com>
2022-10-24 00:52:05 -0700

    upload ke github

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\
+{
+    AuthController,
+    DashboardController,
+    BarangController,
+};
+
+Route::get('/', function () {
+    return redirect('/login');
+});
+
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/postlogin', [AuthController::class, 'postlogin'])->name('postlogin');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::resource('/barang', BarangController::class);
