b9bd32508845af3a6d012b4d2987b31bd11b9dcf
Fari72 <fariaffrizal@gmail.com>
2022-10-24 21:54:46 -0700

    update error

--- resources/views/template/navbar.blade.php
+++ resources/views/template/navbar.blade.php
@@ -9,9 +9,9 @@
       <li class="dropdown"><a href="#" data-toggle="dropdown"
               class="nav-link dropdown-toggle nav-link-lg nav-link-user">
               <img alt="image" src="assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
-              <div class="d-sm-none d-lg-inline-block">Hi, {{Auth()->user()->name}}</div>
+              <div class="d-sm-none d-lg-inline-block">Hi, {{!empty(auth()->user()->name) ? auth()->user()->name : 'administrator'}}</div>
             </a>
-              
+            
           <div class="dropdown-menu dropdown-menu-right">
               <a href="#" class="dropdown-item has-icon">
                   <i class="far fa-user"></i> Profile
